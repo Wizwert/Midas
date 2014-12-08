32f4b2cbdc906b73030bbf828b30ae2addba0b0c
Will <william.rusinko@gmail.com>
2014-12-08 11:33:22 -0600

    Show List of Securities

---
+++ Midas.ObjectModel/Security.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midas.ObjectModel
+{
+    public class Security
+    {
+        private readonly string _securityName;
+        private readonly string _symbol;
+        private readonly string _currency;
+        private readonly string _marketcap;
+        private readonly decimal? _priceToEarningsRatio;
+        private readonly decimal? _dividendYield;
+        private readonly decimal? _priceChangePercent52Week;
+
+        public Security(string securityName, string symbol, string currency, string marketcap, decimal? priceToEarningsRatio, decimal? dividendYield, decimal? priceChangePercent52Week)
+        {
+            _securityName = securityName;
+            _symbol = symbol;
+            _currency = currency;
+            _marketcap = marketcap;
+            _priceToEarningsRatio = priceToEarningsRatio;
+            _dividendYield = dividendYield;
+            _priceChangePercent52Week = priceChangePercent52Week;
+        }
+
+        public string SecurityName
+        {
+            get { return _securityName; }
+        }
+
+        public string Symbol
+        {
+            get { return _symbol; }
+        }
+
+        public string Currency
+        {
+            get { return _currency; }
+        }
+
+        public string Marketcap
+        {
+            get { return _marketcap; }
+        }
+
+        public decimal? PriceToEarningsRatio
+        {
+            get { return _priceToEarningsRatio; }
+        }
+
+        public decimal? DividendYield
+        {
+            get { return _dividendYield; }
+        }
+
+        public decimal? PriceChangePercent52Week
+        {
+            get { return _priceChangePercent52Week; }
+        }
+    }
+}
---
+++ Midas.UI.Wpf/SecuritiesList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Midas.UI.Wpf
+{
+    /// <summary>
+    /// Interaction logic for SecuritiesList.xaml
+    /// </summary>
+    public partial class SecuritiesList : UserControl
+    {
+        private readonly SecuritiesListViewModel _viewModel = new SecuritiesListViewModel();
+
+        public SecuritiesList()
+        {
+            InitializeComponent();
+
+            DataContext = _viewModel;
+        }
+    }
+}
